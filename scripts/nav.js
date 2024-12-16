8406d1278c70944a9a2626cb1ed778eebd1636e5
Protke <a.protic97@gmail.com>
2024-12-16 21:12:18 +0100

    menu close on contact link

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,11 +1,16 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navMenu = document.getElementById("nav-menu");
 const closeMenu = document.getElementById("close-menu");
+const contactLink = document.getElementById("contact-link");
 
 menuToggle.addEventListener("click", () => {
     navMenu.classList.add("open");
 });
 
 closeMenu.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+});
+
+contactLink.addEventListener("click", () => {
     navMenu.classList.remove("open");
 });
